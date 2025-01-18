fda9d0161051f5ab88f7bf631acfa7be66c2288e
Bhargav Malasani <86823772+BhargavMN@users.noreply.github.com>
2025-01-18 17:39:20 +0100

    Init With Timing strategy and PID strategy

---
+++ evry_project_strategy/nodes/agent_strategy2_PID.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist, Pose2D
+from sensor_msgs.msg import Range
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+
+from evry_project_plugins.srv import DistanceToFlag
+
+
+class Robot:
+    def __init__(self, robot_name):
+        """Constructor of the class Robot
+        The required publishers / subscribers are created.
+        The attributes of the class are initialized
+
+        Args:
+            robot_name (str): Name of the robot, like robot_1, robot_2 etc. To be used for your subscriber and publisher with the robot itself
+        """
+        self.speed = 0.0
+        self.angle = 0.0
+        self.sonar = 5.0  # Sonar distance
+        self.x, self.y = 0.0, 0.0  # coordinates of the robot
+        self.yaw = 0.0  # yaw angle of the robot
+        self.robot_name = robot_name
+
+        '''Listener and publisher'''
+
+        rospy.Subscriber(self.robot_name + "/sensor/sonar_front",
+                         Range, self.callbackSonar)
+        rospy.Subscriber(self.robot_name + "/odom",
+                         Odometry, self.callbackPose)
+        self.cmd_vel_pub = rospy.Publisher(
+            self.robot_name + "/cmd_vel", Twist, queue_size=1)
+
+    def callbackSonar(self, msg):
+        """Callback function that gets the data coming from the ultrasonic sensor
+
+        Args:
+            msg (Range): Message that contains the distance separating the US sensor from a potential obstacle
+        """
+        self.sonar = msg.range
+
+    def get_sonar(self):
+        """Method that returns the distance separating the ultrasonic sensor from a potential obstacle
+        """
+        return self.sonar
+
+    def callbackPose(self, msg):
+        """Callback function that gets the data coming from the ultrasonic sensor
+
+        Args:
+            msg (Odometry): Message that contains the coordinates of the agent
+        """
+        self.x = msg.pose.pose.position.x
+        self.y = msg.pose.pose.position.y
+        quaternion = msg.pose.pose.orientation
+        quaternion_list = [quaternion.x,
+                           quaternion.y, quaternion.z, quaternion.w]
+        roll, pitch, yaw = euler_from_quaternion(quaternion_list)
+        self.yaw = yaw
+
+    def get_robot_pose(self):
+        """Method that returns the position and orientation of the robot"""
+        return self.x, self.y, self.yaw
+
+    def constraint(self, val, min=-2.0, max=2.0):
+        """Method that limits the linear and angular velocities sent to the robot
+
+        Args:
+            val (float): [Desired velocity to send
+            min (float, optional): Minimum velocity accepted. Defaults to -2.0.
+            max (float, optional): Maximum velocity accepted. Defaults to 2.0.
+
+        Returns:
+            float: Limited velocity whose value is within the range [min; max]
+        """
+        # DO NOT TOUCH
+        if val < min:
+            return min
+        if val > max:
+            return max
+        return val
+
+    def set_speed_angle(self, linear, angular):
+        """Method that publishes the proper linear and angular velocities commands on the related topic to move the robot
+
+        Args:
+            linear (float): desired linear velocity
+            angular (float): desired angular velocity
+        """
+        cmd_vel = Twist()
+        cmd_vel.linear.x = self.constraint(linear)
+        cmd_vel.angular.z = self.constraint(angular, min=-1, max=1)
+        self.cmd_vel_pub.publish(cmd_vel)
+
+    def getDistanceToFlag(self):
+        """Get the distance separating the agent from a flag. The service 'distanceToFlag' is called for this purpose.
+        The current position of the robot and its id should be specified. The id of the robot corresponds to the id of the flag it should reach
+
+
+        Returns:
+            float: the distance separating the robot from the flag
+        """
+        rospy.wait_for_service('/distanceToFlag')
+        try:
+            service = rospy.ServiceProxy('/distanceToFlag', DistanceToFlag)
+            pose = Pose2D()
+            pose.x = self.x
+            pose.y = self.y
+            # int(robot_name[-1]) corresponds to the id of the robot. It is also the id of the related flag
+            result = service(pose, int(self.robot_name[-1]))
+            return result.distance
+        except rospy.ServiceException as e:
+            print("Service call failed: %s" % e)
+
+
+class PID: 
+
+    def __init__(self,K_p=1.0,K_i=0.0,K_d=0):
+        self.kp=K_p
+        self.ki=K_i
+        self.kd=K_d
+
+        self.sum_error =0.0
+        self.prev_error =0.0
+
+    def PID_Calculator(self,error):
+        self.sum_error += error
+        diff_error = error - self.prev_error
+
+        output= self.kp*error + self.ki*self.sum_error + self.kd*diff_error
+        self.prev_error=error
+        return output
+
+
+
+        
+        
+def run_demo():
+    """Main loop"""
+    robot_name = rospy.get_param("~robot_name")
+    robot = Robot(robot_name)
+    print(f"Robot : {robot_name} is starting..")
+
+    init_flag_dist = robot.getDistanceToFlag()
+    pid= PID(0.1,0.5,0.5)
+
+    while not rospy.is_shutdown():
+        # Strategy 2
+        Obs_dist=robot.get_sonar()
+
+        flag_dist=robot.getDistanceToFlag()
+
+        #speed and angle update
+        speed=pid.PID_Calculator(flag_dist)
+        angle= pid.PID_Calculator(robot.yaw)
+
+        #obstacle detection
+
+        if Obs_dist<0.5:
+            speed=0
+            angle=pid.PID_Calculator(0.5)
+        else :
+            if flag_dist>init_flag_dist:
+                angle=0.03
+            elif flag_dist<2:
+                angle=0
+                speed=0
+            else :
+                angle = -0.1
+
+        
+    
+        # Finishing by publishing the desired speed. 
+        # DO NOT TOUCH.
+        robot.set_speed_angle(speed, angle)
+        rospy.sleep(0.5)
+            
+
+
+if __name__ == "__main__":
+    print("Running ROS..")
+    rospy.init_node("Controller", anonymous=True)
+    Robot_name = rospy.get_param("~robot_name")
+    run_demo()
